dd870ef06a89b13b25d4aafbb414b28e24cf8ceb
Oscar Alberto Mendoza Chavez <omendoza@ypfb.gov.bo>
2019-12-23 15:39:46 -0400

    Commit of the First Plot

---
+++ plot1.R
@@ -0,0 +1,25 @@
+#Plot1.R
+#This plot is a histogram of "Global Active Users".
+
+#First read the data in the .txt file
+
+filename <- "household_power_consumption.txt"
+
+household_data_consumption <- read.table(file = filename, header = TRUE, sep = ";",  na.strings = "?")
+
+#Apply as.Date(). The date format is %d/%m%/%Y
+
+household_data_consumption$Date <- as.Date(household_data_consumption$Date, "%d/%m/%Y")
+
+#Subset the data between 2007-02-01 and 2007-02-02
+
+HDC_subset <- subset(household_data_consumption, Date == "2007-02-01" | Date == "2007-02-02")
+
+#remove the original dataset to free RAM
+
+rm(household_data_consumption)
+
+#Plot the histogram
+png(file = "plot1.png", width = 480, height = 480)
+hist(x = HDC_subset$Global_active_power, col = "red", xlab = "Global Active Power (kilowatts)", main = "Global Active Power")
+dev.off()
